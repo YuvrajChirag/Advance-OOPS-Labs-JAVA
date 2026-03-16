3d93dfc0c74e4433dde30ffe6d13f041f3e6c9e7
Yuvraj Chirag <yuvrajchirag9se@gmail.com>
2026-03-17 00:04:59 +0530

    Update BFSTest.java

--- Lab-01/BFSTest.java
+++ Lab-01/BFSTest.java
@@ -11,5 +11,12 @@ public class BFSTest {
         g.addEdge(3, 4);
 
         g.bfs(2);
+
+        Graph g1 = new Graph(4);
+
+        g1.addEdge(0,1);
+        g1.addEdge(1,3);
+        g1.addEdge(2,4);
+        g1.addEdge(3,4);
     }
 }
